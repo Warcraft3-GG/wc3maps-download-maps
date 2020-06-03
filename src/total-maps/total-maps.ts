63831dd450a6c4628bf2ae98d8fd46390dfc9264
Sansossio <juliosansossio@gmail.com>
2020-06-03 12:12:32 +0200

    Limit 0 on total maps

--- src/total-maps/total-maps.ts
+++ src/total-maps/total-maps.ts
@@ -4,6 +4,6 @@ import { wc3mapsApicall } from '../api/api.wc3maps'
  * Return total maps in wc3maps.com
  */
 export async function totalMaps () {
-  const { total } = await wc3mapsApicall('search')
+  const { total } = await wc3mapsApicall('search', { limit: 0 })
   return total
 }
