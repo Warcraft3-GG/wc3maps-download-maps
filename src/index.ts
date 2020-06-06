7a9237328648700d034a3fc22a5efffa360950af
Sansossio <juliosansossio@gmail.com>
2020-06-06 19:24:03 +0200

    export get map function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { totalMaps } from './total-maps/total-maps'
 export { getMapsIDPerPage } from './get-maps-id-per-page/get-maps-id-per-page'
 export { getTotalPages } from './get-total-pages/get-total-pages'
+export { getMap } from './get-map/get-map'
