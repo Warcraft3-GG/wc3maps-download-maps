5ece96120aa32485ee5064e518d26d9f23cadea1
Sansossio <juliosansossio@gmail.com>
2020-06-06 19:37:05 +0200

    Get map example

---
+++ examples/get-map.example.ts
@@ -0,0 +1,9 @@
+import { getMap } from '../src'
+
+(async () => {
+  // Map: https://wc3maps.com/map/43/DBZ_Tribute_Elite_v2.1c
+  const id = 43
+  const download = false
+  const map = await getMap(id, download)
+  console.log(map)
+})()
